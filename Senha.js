4cc8d784b4d129d10e9085e182388f8b3cd5ff66
MarcosViniSil <Marcossilv203@gmail.com>
2023-01-19 18:07:55 -0300

    .

--- Senha.js
+++ Senha.js
@@ -23,9 +23,9 @@ function clic() {
             for (let i = 0; i < Number(quant.value); i++) {
                 posicao = parseInt(Math.random() * 62);
                 senha += opcao2[posicao];
-                
+
             }
-            
+
 
         } else if (numeros === "Nao" && simbolo === "Sim") { //com simbolos e sem números
             var opcao3 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
@@ -37,7 +37,7 @@ function clic() {
                 senha += opcao3[posicao];
                 console.log(opcao1[posicao]);
             }
-            
+
         } else if (numeros === "Sim" && simbolo === "Sim") { //ambos,símbolos e números
             var opcao4 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
                 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "@", "#", "$", "%", "&", "*", "/", "+", "-", "="];
@@ -46,11 +46,17 @@ function clic() {
                 posicao = parseInt(Math.random() * 72);
                 senha += opcao4[posicao];
             }
-          
+
         }
 
         resul.innerHTML = senha;
         quant.value = "";
+        for (var i = 0; i < numeros.length; i++){
+            numeros[i].checked = false;
+
+        }
+            
+        simbolo.innerHTML = "";
     } else {
         alert("A quantidade de caracteres deve ser entre 8 e 20");
     }
